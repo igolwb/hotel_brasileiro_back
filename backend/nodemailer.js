2ba53d7e4fdce9095f98bfc3616cd9433378813c
@_Ingoat <igolwb@gmail.com>
2025-10-31 10:45:42 -0300

    Update nodemailer.js

--- backend/nodemailer.js
+++ backend/nodemailer.js
@@ -8,7 +8,10 @@ const sendTokenEmail = async (email, token) => {
 		auth: {
 			user: process.env.EMAIL_USER,
 			pass: process.env.EMAIL_PASS
-		}
+		},
+		  connectionTimeout: 10000, // Increase timeout to 10 seconds
+		    debug: true, // Enable debug output
+  			logger: true //
 	});
 
 	const mailOptions = {
